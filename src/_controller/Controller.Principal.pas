c956699c0cbe944c98c3e7d3f5a31df2f5aa7f8e
Gabriel-Braga-03 <47041885+Gabriel-Braga-03@users.noreply.github.com>
2020-05-01 15:04:06 -0300

    Commit 01-05-20
    + Criação de arquivos atualizada.

---
+++ src/_controller/Controller.Principal.pas
@@ -0,0 +1,15 @@
+unit Controller.Principal;
+
+interface
+    uses
+        Model.Horario,
+        Model.FileManager;
+
+    // Variáveis de Controle
+    var
+        fm: TFileManager;
+        horario: THorario;
+
+implementation
+
+end.
